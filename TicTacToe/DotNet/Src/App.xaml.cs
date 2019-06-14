650ff4ed93439ad8f7c7c8ae058d04cbf59ff97b
ObiWanLansi <joerg.lanser@gmail.com>
2019-06-14 20:54:27 +0200

    TicTacToe WPF

---
+++ TicTacToe/DotNet/Src/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ TicTacToe/DotNet/Src/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
