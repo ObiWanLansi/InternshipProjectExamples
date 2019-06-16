b53ea82c1a2cb70e477ef05c6af45597af53d109
ObiWanLansi <joerg.lanser@gmail.com>
2019-06-16 23:40:27 +0200

    Calendar

--- Calendar/DotNet/Console/Src/Calendar.cs
+++ Calendar/DotNet/Console/Src/Calendar.cs
@@ -1,39 +1,119 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+
 
 namespace Calendar
 {
     /// <summary>
     /// A Simple Consolen Calendar
     /// </summary>
-    class Calendar
+    internal class Calendar
     {
+        /// <summary>
+        /// The GregorianCalendar.
+        /// </summary>
+        private static readonly GregorianCalendar CAL = new GregorianCalendar();
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Gets the day of week.
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <returns></returns>
+        private static int GetDayOfWeek(DayOfWeek dow)
+        {
+            int iValue = (int)dow - 1;
+            return iValue < 0 ? 6 : iValue;
+        }
+
+
+        /// <summary>
+        /// Week of the date.
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <returns></returns>
+        private static int WeekOfDate(DateTime dt)
+        {
+            int week = CAL.GetWeekOfYear(dt, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            return week > 0 && week < 53 ? week : 1;
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Displays the calendar.
+        /// </summary>
+        /// <param name="iYear">The i year.</param>
+        /// <param name="iMonth">The i month.</param>
+        private static void DisplayCalendar(int iYear, int iMonth)
+        {
+            DateTime dtFirstDayInMonth = new DateTime(iYear, iMonth, 1);
+            Console.Out.WriteLine("┌───────────────────────────┐");
+            Console.Out.WriteLine("│ {0,25} │", string.Format("{0} {1}", dtFirstDayInMonth.ToString("MMMMM"), iYear));
+            Console.Out.WriteLine("├────┬──────────────────────┤");
+            Console.Out.WriteLine("│ KW │ MO DI MI DO FR SA SO │");
+            Console.Out.WriteLine("├────┬──────────────────────┤");
+
+            int iOffset = GetDayOfWeek(dtFirstDayInMonth.DayOfWeek);
+            int iDaysInMonth = DateTime.DaysInMonth(iYear, iMonth);
+            int iCalendarWeekStart = WeekOfDate(dtFirstDayInMonth);
+            int iColumnCounter = iOffset;
+
+            Console.Out.Write("│ {0,2} │", iCalendarWeekStart);
+
+            for (int iOffsetCount = 0; iOffsetCount < iOffset; iOffsetCount++)
+            {
+                Console.Out.Write("   ");
+            }
+
+            for (int iDayOfTheMonth = 1; iDayOfTheMonth < iDaysInMonth + 1; iDayOfTheMonth++)
+            {
+                Console.Out.Write(" {0,2}", iDayOfTheMonth);
+
+                if (iDayOfTheMonth < iDaysInMonth)
+                {
+                    if (++iColumnCounter > 6)
+                    {
+                        Console.Out.WriteLine(" │");
+                        int iCalendarWeek = iCalendarWeekStart + ((iDayOfTheMonth + iOffset) / 7);
+                        Console.Out.Write("│ {0,2} │", iCalendarWeek < 53 ? iCalendarWeek : 1);
+                        iColumnCounter = 0;
+                    }
+                }
+            }
+
+            for (int iOffsetCount = 0; iOffsetCount < 6 - iColumnCounter; iOffsetCount++)
+            {
+                Console.Out.Write("   ");
+            }
+
+            Console.Out.WriteLine(" │");
+            Console.Out.WriteLine("└────┴──────────────────────┘");
+            Console.Out.WriteLine("");
+        }
+
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            DateTime dtToday = DateTime.Now;
+            DateTime dt = DateTime.Now;
 
-            //DateTime dtToday = new DateTime(2018, 1, 1);
+            DisplayCalendar(dt.Year, dt.Month - 1);
+            DisplayCalendar(dt.Year, dt.Month);
+            DisplayCalendar(dt.Year, dt.Month + 1);
 
-            //                              1         2
-            //                     12345678901234567890123456789
-            Console.Out.WriteLine("┌───────────────────────────┐");
-            Console.Out.WriteLine("│ {0,25} │", string.Format("{0} {1}", dtToday.ToString("MMMMM"), dtToday.Year));
-            Console.Out.WriteLine("├────┬──────────────────────┤");
-            Console.Out.WriteLine("│ KW │ MO DI MI DO FR SA SO │");
-            Console.Out.WriteLine("├────┬──────────────────────┤");
-            Console.Out.WriteLine("│ XX │ XX XX XX XX XX XX XX │");
-            Console.Out.WriteLine("│ XX │ XX XX XX XX XX XX XX │");
-            Console.Out.WriteLine("│ XX │ XX XX XX XX XX XX XX │");
-            Console.Out.WriteLine("│ XX │ XX XX XX XX XX XX XX │");
-            Console.Out.WriteLine("│ XX │ XX XX XX XX XX XX XX │");
-            Console.Out.WriteLine("└────┴──────────────────────┘");
-            Console.Out.WriteLine("");
-            Console.Out.WriteLine("");
-
-            Console.ReadKey(false);
+            if (Debugger.IsAttached)
+            {
+                Console.ReadKey(false);
+            }
         }
     }
 }
---
+++ Calendar/DotNet/WPF/Src/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// The GregorianCalendar.
+        /// </summary>
+        private static readonly GregorianCalendar CAL = new GregorianCalendar();
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Gets the day of week.
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <returns></returns>
+        private static int GetDayOfWeek(DayOfWeek dow)
+        {
+            int iValue = (int)dow - 1;
+            return iValue < 0 ? 6 : iValue;
+        }
+
+
+        /// <summary>
+        /// Week of the date.
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <returns></returns>
+        private static int WeekOfDate(DateTime dt)
+        {
+            int week = CAL.GetWeekOfYear(dt, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            return week > 0 && week < 53 ? week : 1;
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetupGUI(DateTime.Now);
+            //SetupGUI(new DateTime(2019, 12, 24));
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Setups the GUI.
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        private void SetupGUI(DateTime dt)
+        {
+            this.Title = $"{dt.ToString("MMMMM")} {dt.Year} ";
+
+            DateTime dtFirstDayInMonth = new DateTime(dt.Year, dt.Month, 1);
+
+            int iOffset = GetDayOfWeek(dtFirstDayInMonth.DayOfWeek);
+            int iDaysInMonth = DateTime.DaysInMonth(dt.Year, dt.Month);
+            int iCalendarWeekStart = WeekOfDate(dtFirstDayInMonth);
+
+            int iColumnCounter = iOffset;
+            int iRowCounter = 1;
+
+            this.grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+            Label lCwFc = new Label
+            {
+                Content = iCalendarWeekStart,
+                Background = Brushes.Aqua,
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(1),
+                Margin = new Thickness(1),
+                HorizontalContentAlignment = HorizontalAlignment.Right
+            };
+
+            this.grid.Children.Add(lCwFc);
+
+            Grid.SetColumn(lCwFc, 0);
+            Grid.SetRow(lCwFc, 1);
+
+            for (int iDayOfTheMonth = 1; iDayOfTheMonth < iDaysInMonth + 1; iDayOfTheMonth++)
+            {
+                Label l = new Label
+                {
+                    Content = iDayOfTheMonth,
+                    Background = (iColumnCounter == 5 || iColumnCounter == 6) ? Brushes.DeepSkyBlue : Brushes.SkyBlue,
+                    BorderBrush = Brushes.Black,
+                    BorderThickness = new Thickness(1),
+                    Margin = new Thickness(1),
+                    HorizontalContentAlignment = HorizontalAlignment.Right
+                };
+
+                this.grid.Children.Add(l);
+
+                Grid.SetColumn(l, iColumnCounter + 1);
+                Grid.SetRow(l, iRowCounter);
+
+                if (iDayOfTheMonth < iDaysInMonth)
+                {
+                    if (++iColumnCounter > 6)
+                    {
+                        iColumnCounter = 0;
+                        iRowCounter++;
+
+                        int iCalendarWeek = iCalendarWeekStart + ((iDayOfTheMonth + iOffset) / 7);
+
+                        this.grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+                        Label lWeek = new Label
+                        {
+                            Content = iCalendarWeek < 53 ? iCalendarWeek : 1,
+                            Background = Brushes.Aqua,
+                            BorderBrush = Brushes.Black,
+                            BorderThickness = new Thickness(1),
+                            Margin = new Thickness(1),
+                            HorizontalContentAlignment = HorizontalAlignment.Right
+                        };
+
+                        this.grid.Children.Add(lWeek);
+
+                        Grid.SetColumn(lWeek, 0);
+                        Grid.SetRow(lWeek, iRowCounter);
+                    }
+                }
+            }
+        }
+    }
+}
