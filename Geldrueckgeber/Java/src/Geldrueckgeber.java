1ae7a99c59171ae7c6d1a30ef76a1ecdd231f629
Jörg Lanser <joerg.lanser@gmail.com>
2019-06-16 13:34:39 +0200

    Geldrückgeber

--- Geldrueckgeber/Java/src/Geldrueckgeber.java
+++ Geldrueckgeber/Java/src/Geldrueckgeber.java
@@ -1,36 +1,59 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+
+class BillOrCoin {
+
+	final public int iValue;
+	final public String strDisplayName;
+
+	public BillOrCoin(int iValue, String strDisplayName) {
+
+		this.iValue = iValue;
+		this.strDisplayName = strDisplayName;
+	}
+}
 
 public class Geldrueckgeber {
 
 	/**
-	 * Alle Münzen und Scheine mit ihrem Wert in Cent die wir zur Auszahlung verwenden können.
+	 * Alle Münzen und Scheine mit ihrem Wert in Cent die wir zur Auszahlung
+	 * verwenden können.
 	 */
-	final private static ArrayList<Double> lBillsAndCoins = new ArrayList<Double>();
-	
+	final private static ArrayList<BillOrCoin> lBillsAndCoins = new ArrayList<BillOrCoin>();
+
 	static {
 
-		lBillsAndCoins.add(50000d); // Fünfhundert Euro
-		lBillsAndCoins.add(20000d); // zweihundert Euro
-		lBillsAndCoins.add(10000d); // Hundert Euro
+		lBillsAndCoins.add(new BillOrCoin(50000, "Fünfhundert Euro"));
+		lBillsAndCoins.add(new BillOrCoin(20000, "Zweihundert Euro"));
+		lBillsAndCoins.add(new BillOrCoin(10000, "Hundert Euro"));
 
-		lBillsAndCoins.add(5000d); // Fünfzig Euro
-		lBillsAndCoins.add(2000d); // Zwanzig Euro
-		lBillsAndCoins.add(1000d); // Zehn Euro
+		lBillsAndCoins.add(new BillOrCoin(5000, "Fünfzig Euro"));
+		lBillsAndCoins.add(new BillOrCoin(2000, "Zwanzig Euro"));
+		lBillsAndCoins.add(new BillOrCoin(1000, "Zehn Euro"));
 
-		lBillsAndCoins.add(500d); // Fünf Euro
-		lBillsAndCoins.add(200d); // Zwei Euro
-		lBillsAndCoins.add(100d); // Ein Euro
+		lBillsAndCoins.add(new BillOrCoin(500, "Fünf Euro"));
+		lBillsAndCoins.add(new BillOrCoin(200, "Zwei Euro"));
+		lBillsAndCoins.add(new BillOrCoin(100, "Ein Euro"));
 
-		lBillsAndCoins.add(50d); // Fünfzig Cent
-		lBillsAndCoins.add(20d); // Zwanzig Cent
-		lBillsAndCoins.add(10d); // Zehn Cent
+		lBillsAndCoins.add(new BillOrCoin(50, "Fünfzig Cent"));
+		lBillsAndCoins.add(new BillOrCoin(20, "Zwanzig Cent"));
+		lBillsAndCoins.add(new BillOrCoin(10, "Zehn Cent"));
 
-		lBillsAndCoins.add(5d); // Fünf Cent
-		lBillsAndCoins.add(2d); // Zwei Cent
-		lBillsAndCoins.add(1d); // Ein Cent
+		lBillsAndCoins.add(new BillOrCoin(5, "Fünf Cent"));
+		lBillsAndCoins.add(new BillOrCoin(2, "Zwei Cent"));
+		lBillsAndCoins.add(new BillOrCoin(1, "Ein Cent"));
 	}
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * 
+	 * @param fAmount
+	 * @return
+	 */
+	private static HashMap<BillOrCoin, Integer> getBillsAndCoinsCount(float fAmount) {
+		return null;
+	}
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -39,10 +62,14 @@ public class Geldrueckgeber {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		float fAmount = (float) 88.88;
-		
-		
 
+		float fAmount = 88.88f;
+
+		var result = getBillsAndCoinsCount(fAmount);
+
+		for (BillOrCoin boc : result.keySet()) {
+
+			System.out.println(String.format("{0}: {1}", boc.strDisplayName, result.get(boc)));
+		}
 	}
-
 }
